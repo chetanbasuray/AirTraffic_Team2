2502dc05ae64e491c9ddb0cf931eebb2ddcc8690
Chetan Basuray <urwithchetan@gmail.com>
2015-06-14 11:34:40 +0200

    bean attributes

--- AirTraffic_Team2/src/com/AirTraffic/Team2/Models/CateringBean.java
+++ AirTraffic_Team2/src/com/AirTraffic/Team2/Models/CateringBean.java
@@ -8,5 +8,56 @@ package com.AirTraffic.Team2.Models;
  *
  */
 public class CateringBean {
-
+private int catering_Id;
+private String catering_type;
+private double catering_price;
+private String catering_drink;
+/**
+ * @return the catering_Id
+ */
+public int getCatering_Id() {
+	return catering_Id;
+}
+/**
+ * @param catering_Id the catering_Id to set
+ */
+public void setCatering_Id(int catering_Id) {
+	this.catering_Id = catering_Id;
+}
+/**
+ * @return the catering_type
+ */
+public String getCatering_type() {
+	return catering_type;
+}
+/**
+ * @param catering_type the catering_type to set
+ */
+public void setCatering_type(String catering_type) {
+	this.catering_type = catering_type;
+}
+/**
+ * @return the catering_price
+ */
+public double getCatering_price() {
+	return catering_price;
+}
+/**
+ * @param catering_price the catering_price to set
+ */
+public void setCatering_price(double catering_price) {
+	this.catering_price = catering_price;
+}
+/**
+ * @return the catering_drink
+ */
+public String getCatering_drink() {
+	return catering_drink;
+}
+/**
+ * @param catering_drink the catering_drink to set
+ */
+public void setCatering_drink(String catering_drink) {
+	this.catering_drink = catering_drink;
+}
 }
--- AirTraffic_Team2/src/com/AirTraffic/Team2/Models/CounterBean.java
+++ AirTraffic_Team2/src/com/AirTraffic/Team2/Models/CounterBean.java
@@ -3,10 +3,102 @@
  */
 package com.AirTraffic.Team2.Models;
 
+import java.util.Date;
+
 /**
  * @author JOY
  *
  */
 public class CounterBean {
-
+private int counter_id;
+private String counter_type;
+private Date counter_opening_time;
+private Date counter_closing_time;
+private boolean counter_hasCash;
+private String counter_status;
+private TerminalBean terminalBean;
+/**
+ * @return the counter_id
+ */
+public int getCounter_id() {
+	return counter_id;
+}
+/**
+ * @param counter_id the counter_id to set
+ */
+public void setCounter_id(int counter_id) {
+	this.counter_id = counter_id;
+}
+/**
+ * @return the counter_type
+ */
+public String getCounter_type() {
+	return counter_type;
+}
+/**
+ * @param counter_type the counter_type to set
+ */
+public void setCounter_type(String counter_type) {
+	this.counter_type = counter_type;
+}
+/**
+ * @return the counter_opening_time
+ */
+public Date getCounter_opening_time() {
+	return counter_opening_time;
+}
+/**
+ * @param counter_opening_time the counter_opening_time to set
+ */
+public void setCounter_opening_time(Date counter_opening_time) {
+	this.counter_opening_time = counter_opening_time;
+}
+/**
+ * @return the counter_closing_time
+ */
+public Date getCounter_closing_time() {
+	return counter_closing_time;
+}
+/**
+ * @param counter_closing_time the counter_closing_time to set
+ */
+public void setCounter_closing_time(Date counter_closing_time) {
+	this.counter_closing_time = counter_closing_time;
+}
+/**
+ * @return the counter_hasCash
+ */
+public boolean isCounter_hasCash() {
+	return counter_hasCash;
+}
+/**
+ * @param counter_hasCash the counter_hasCash to set
+ */
+public void setCounter_hasCash(boolean counter_hasCash) {
+	this.counter_hasCash = counter_hasCash;
+}
+/**
+ * @return the counter_status
+ */
+public String getCounter_status() {
+	return counter_status;
+}
+/**
+ * @param counter_status the counter_status to set
+ */
+public void setCounter_status(String counter_status) {
+	this.counter_status = counter_status;
+}
+/**
+ * @return the terminalBean
+ */
+public TerminalBean getTerminalBean() {
+	return terminalBean;
+}
+/**
+ * @param terminalBean the terminalBean to set
+ */
+public void setTerminalBean(TerminalBean terminalBean) {
+	this.terminalBean = terminalBean;
+}
 }
