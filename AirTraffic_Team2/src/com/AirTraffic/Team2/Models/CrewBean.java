41fe00af15f7a9463a021578170556acc0f96e20
Chetan Basuray <urwithchetan@gmail.com>
2015-06-14 11:42:20 +0200

    beans

--- AirTraffic_Team2/src/com/AirTraffic/Team2/Models/CrewBean.java
+++ AirTraffic_Team2/src/com/AirTraffic/Team2/Models/CrewBean.java
@@ -3,10 +3,50 @@
  */
 package com.AirTraffic.Team2.Models;
 
+import java.util.Date;
+
 /**
  * @author JOY
  *
  */
 public class CrewBean {
-
+private int crew_Id;
+private Date crew_start_working_time;
+private Date crew_end_working_time;
+/**
+ * @return the crew_Id
+ */
+public int getCrew_Id() {
+	return crew_Id;
+}
+/**
+ * @param crew_Id the crew_Id to set
+ */
+public void setCrew_Id(int crew_Id) {
+	this.crew_Id = crew_Id;
+}
+/**
+ * @return the crew_start_working_time
+ */
+public Date getCrew_start_working_time() {
+	return crew_start_working_time;
+}
+/**
+ * @param crew_start_working_time the crew_start_working_time to set
+ */
+public void setCrew_start_working_time(Date crew_start_working_time) {
+	this.crew_start_working_time = crew_start_working_time;
+}
+/**
+ * @return the crew_end_working_time
+ */
+public Date getCrew_end_working_time() {
+	return crew_end_working_time;
+}
+/**
+ * @param crew_end_working_time the crew_end_working_time to set
+ */
+public void setCrew_end_working_time(Date crew_end_working_time) {
+	this.crew_end_working_time = crew_end_working_time;
+}
 }
