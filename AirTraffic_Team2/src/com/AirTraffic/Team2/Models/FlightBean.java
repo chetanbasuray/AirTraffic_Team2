fd196a18794f7ea269c5f1db8a4c1c1aadbf2fa2
ahmed <ahmed@ahmed-VirtualBox>
2015-07-21 14:43:49 +0200

    changes

--- AirTraffic_Team2/src/com/AirTraffic/Team2/Models/FlightBean.java
+++ AirTraffic_Team2/src/com/AirTraffic/Team2/Models/FlightBean.java
@@ -32,9 +32,6 @@ private int flight_first_class_cabin_luggage_limit;
 private String seatNumber;
 private String seatClass;
 
-<<<<<<< HEAD
-=======
-
 /**
  * @return the seatNumber
  */
@@ -59,21 +56,7 @@ public String getSeatClass() {
 public void setSeatClass(String seatClass) {
   this.seatClass = seatClass;
 }
-public Timestamp getFlight_scheduled_departure_timestamp() {
-	return flight_scheduled_departure_timestamp;
-}
-public void setFlight_scheduled_departure_timestamp(
-		Timestamp flight_scheduled_departure_timestamp) {
-	this.flight_scheduled_departure_timestamp = flight_scheduled_departure_timestamp;
-}
-public Timestamp getFlight_scheduled_arrival_timestamp() {
-	return flight_scheduled_arrival_timestamp;
-}
-public void setFlight_scheduled_arrival_timestamp(
-		Timestamp flight_scheduled_arrival_timestamp) {
-	this.flight_scheduled_arrival_timestamp = flight_scheduled_arrival_timestamp;
-}
->>>>>>> 028201e187d3f8b875ccfa5cc317e476dae78c22
+
 private double flight_first_class_price;
 private double flight_business_class_price;
 private double flight_economy_class_price;
