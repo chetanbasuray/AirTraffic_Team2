ec7c2ba9ba3c20f0d23afe75597c6c6aee3b11bb
Adnan Rabbi <rabbi@sec.in.tum.de>
2015-07-22 17:29:01 +0200

    Validation search flight

--- AirTraffic_Team2/src/com/AirTraffic/Team2/dao/AbstractDAO.java
+++ AirTraffic_Team2/src/com/AirTraffic/Team2/dao/AbstractDAO.java
@@ -12,6 +12,6 @@ public abstract class AbstractDAO {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		};
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/IntegratedSchema", "postgres", "matchbox91");
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/airtraffic_final", "postgres", "adnan25");
     }
 }
